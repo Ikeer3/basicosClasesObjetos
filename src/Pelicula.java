173028a9ba68c7769c1b75938bd3cd83c115cefe
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-13 13:15:10 +0100

    Implementada clase Pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Pelicula {
+
+    private String titulo;
+    private String director;
+    private int anhoEstreno;
+    private int duracionMinutos;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String tituloEntrada) {
+        titulo = tituloEntrada;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String directorEntrada) {
+        director = directorEntrada;
+    }
+
+    public int getAnhoEstreno() {
+        return anhoEstreno;
+    }
+
+    public void setAnhoEstreno(int anhoEstrenoEntrada) {
+        anhoEstreno = anhoEstrenoEntrada;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutosEntrada) {
+        duracionMinutos = duracionMinutosEntrada;
+    }
 }
