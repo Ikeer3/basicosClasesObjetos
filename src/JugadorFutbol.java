46e61dfde4b14c88e30e9be7885e5cd97809a2bb
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-15 12:15:18 +0100

    Implementada clase JugadorFutbol

---
+++ src/JugadorFutbol.java
@@ -0,0 +1,39 @@
+public class JugadorFutbol {
+
+    private String nombre;
+    private String equipo;
+    private int dorsal;
+    private int posicion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombreEntrada) {
+        nombre = nombreEntrada;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipoEntrada) {
+        equipo = equipoEntrada;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsalEntrada) {
+        dorsal = dorsalEntrada;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicionEntrada) {
+        posicion = posicionEntrada;
+    }
+}
