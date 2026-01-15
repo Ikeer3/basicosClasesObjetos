2687a23516b6673cd6159a3dea18125a5591c429
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-15 13:20:48 +0100

    Implementada clase Skin

---
+++ src/Skin.java
@@ -0,0 +1,39 @@
+public class Skin {
+
+    private String nombre;
+    private String videojuego;
+    private String rareza;
+    private int precioMonedas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombreEntrada) {
+        nombre = nombreEntrada;
+    }
+
+    public String getVideojuego() {
+        return videojuego;
+    }
+
+    public void setVideojuego(String videjuegoEntrada) {
+        videojuego = videjuegoEntrada;
+    }
+
+    public String getRareza() {
+        return rareza;
+    }
+
+    public void setRareza(String rarezaEntrada) {
+        rareza = rarezaEntrada;
+    }
+
+    public int getPrecioMonedas() {
+        return precioMonedas;
+    }
+
+    public void setPrecioMonedas(int precioMonedasEntrada) {
+        precioMonedas = precioMonedasEntrada;
+    }
+}
