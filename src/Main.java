0f606b13e20a8e2ee428242b44f5f3023aa2f485
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-19 12:00:08 +0100

    Ejercicio 9 básicos Objetos actualizado

--- src/Main.java
+++ src/Main.java
@@ -211,5 +211,20 @@ public class Main {
 
         System.out.println("\nDespués de la victoria de " + equipo1.getNombre() + " en el europeo de " + equipo1.getJuego()
                 + ", ha ascendido hasta el ránking " + equipo1.getRankingMundial() + " mundial!!");
+
+        // ESTA PARTE PRUEBA LA CLASE CANCIÓN STATS
+
+        CancionStats cancionStats1 = new CancionStats("Radioactive");
+
+        System.out.println("\n---------------------------------------------------------------------------------------");
+        System.out.println("\nAntes del lanzamiento de " + cancionStats1.getTitulo() + " el número de reproducciones era: "
+                + cancionStats1.getNumReproducciones());
+
+        for (int i = 0; i < 1000; i++) {
+            cancionStats1.reproducir();
+        }
+
+        System.out.println("Tras su éxito, el número total de reproducciones de " + cancionStats1.getTitulo() +
+                " fue: " + cancionStats1.getNumReproducciones());
     }
 }
